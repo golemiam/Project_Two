ef1d7fb6094db4eaf237ff34fbe26c5c4177d068
Armando C. <110721337+RocketBunnyV3@users.noreply.github.com>
2024-10-16 17:38:46 -0600

    Add files via upload

--- BasicSets.java
+++ BasicSets.java
@@ -7,24 +7,65 @@ import java.util.List;
 public class BasicSets {
 	
 	/**
-	 * The complement method gives the expected output, but the process is different from the textbook.
-	 * Loops through the original set and creates a complement/negated set.
-	 * If an element in the original set is 1, a 0 is added to the complement set.
-	 * If an element in the original set is 0, a 1 is added to the complement set.
+	 * Creates differenced sets of A-B and B-A, then Unionizes the two differenced sets.
+	 * @param firstSet
+	 * @param secondSet
+	 * @return
+	 */
+	public static List<Integer> symmetricDifference(List<Integer> firstSet, List<Integer> secondSet) {
+		List<Integer> firstDifference = difference(firstSet, secondSet);
+		List<Integer> secondDifference = difference(secondSet,firstSet);
+		List<Integer> sdSet = union(firstDifference,secondDifference);
+		return sdSet;
+	}
+	
+	/**
+	 * Finds the intersecting elements of the first and second set,
+	 * then subtracts the intersecting elements from the first set.
+	 * 
+	 * If an element exists in both sets,
+	 * A 0 is added to the differenced set.
+	 * A 1 is added to the differenced set if the element only exists in the first set.
+	 * @param firstSet
+	 * @param secondSet
+	 * @return
+	 */
+	public static List<Integer> difference(List<Integer> firstSet, List<Integer> secondSet) {
+		List<Integer> differencedSet = new ArrayList<Integer>();
+		List<Integer> intersectingElements = intersect(firstSet, secondSet);
+		for (int i = 0; i < firstSet.size(); i++) {
+			if (firstSet.get(i) == intersectingElements.get(i)) {
+				differencedSet.add(0);
+			}
+			else {
+				differencedSet.add(1);
+			}
+		}
+		return differencedSet;
+	}
+	
+	
+	/**
+	 * Complement method now aligns with textbook definition of complement sets.
+	 * Finds the difference of the universal set and the original set.
 	 * @param set
 	 * @return
 	 */
-	public static List<Integer> complement(List<Integer> originalSet) {
-		List<Integer> complementSet = new ArrayList<Integer>();
-		for (Integer i: originalSet) {
-			if (i == 1) {
-				complementSet.add(0);
-			}
-			else {
-				complementSet.add(1);
-			}
-		}
+	public static List<Integer> complement(List<Integer> UniversalSet, List<Integer> originalSet) {
+		List<Integer> complementSet = difference(UniversalSet, originalSet);
 		return complementSet;
+		
+//		Old complement code - just in case
+//		List<Integer> complementSet = new ArrayList<Integer>();
+//		for (Integer i: originalSet) {
+//			if (i == 1) {
+//				complementSet.add(0);
+//			}
+//			else {
+//				complementSet.add(1);
+//			}
+//		}
+//		return complementSet;
 	}
 	
 	/**
@@ -69,18 +110,25 @@ public class BasicSets {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	 List<Integer> UniversalSet = new ArrayList<Integer>();
-	 UniversalSet.add(0);
-	 UniversalSet.add(1);
-	 UniversalSet.add(2);
-	 UniversalSet.add(3);
-	 UniversalSet.add(4);
-	 UniversalSet.add(5);
-	 UniversalSet.add(6);
-	 UniversalSet.add(7);
-	 UniversalSet.add(8);
-	 UniversalSet.add(9);
-	 System.out.println(UniversalSet);
+	 List<Integer> originalSet = new ArrayList<Integer>();
+	 originalSet.add(0);
+	 originalSet.add(1);
+	 originalSet.add(2);
+	 originalSet.add(3);
+	 originalSet.add(4);
+	 originalSet.add(5);
+	 originalSet.add(6);
+	 originalSet.add(7);
+	 originalSet.add(8);
+	 originalSet.add(9);
+	 System.out.println(originalSet);
+	 
+	 //U set: {1,1,1,1,1,1,1,1,1,1}
+	 List<Integer> universalSet = new ArrayList<Integer>();
+	 
+	 for (Integer i: originalSet) {
+		 universalSet.add(1);
+	 }
 	 
 	 //Every element in Set A and B are in the Universal Set. Therefore A and B are a subset of U.
 	 
@@ -109,11 +157,15 @@ public class BasicSets {
 	 System.out.println();
 	 
 	 //Not(A) : {7,8,9}
-	 System.out.println("not(A) : " + complement(SetA));
+	 System.out.println("not(A) : " + complement(universalSet, SetA));
 	 //A U B : {0,1,2,3,4,5,6,7,8,9}
 	 System.out.println("A U B : " + union(SetA, SetB));
 	 //A Interset B : {3,4,5,6}
 	 System.out.println("A Interset B : " + intersect(SetA, SetB));
+	 //A Difference B: {0,1,2}
+	 System.out.println("A difference B: " + difference(SetA, SetB));
+	 // A SymmetricDifference B: {0,1,2,7,8,9}
+	 System.out.println("A SymmetricDifference B: " + symmetricDifference(SetA, SetB));
 	}
 
 }
